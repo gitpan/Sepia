1339eef81049848ee5cbac59d54795d1130bb39a
Sean O'Rourke <seano@cpan.org>
2009-07-08 01:16:23 +0000

    Import of SEANO/Sepia-0.99_01 from CPAN.
    gitpan-cpan-distribution: Sepia
    gitpan-cpan-version:      0.99_01
    gitpan-cpan-path:         SEANO/Sepia-0.99_01.tar.gz
    gitpan-cpan-author:       SEANO
    gitpan-cpan-maturity:     developer

--- sepia.el
+++ sepia.el
@@ -44,7 +44,7 @@
 Useful values include `sepia-w3m-view-pod' and `sepia-perldoc-buffer'.")
 
 (defvar sepia-module-list-function
-  (if (featurep 'w3m) 'w3m-find-file 'browse-url-of-buffer)
+  (if (featurep 'w3m) 'w3m-find-file 'browse-url-of-file)
 "* Function to view a list of installed modules.
 
 Useful values include `w3m-find-file' and `browse-url-of-buffer'.")
@@ -731,6 +731,28 @@ The prefix argument is the same as for `end-of-defun'."
       (backward-char 1)
       (forward-sexp))
     (point)))
+
+(defun sepia-rename-lexical (old new &optional prompt)
+  "Replace lexical variable OLD with NEW in the current function.
+
+With prefix argument, query for each replacement.  It is an error
+to call this outside a function."
+  (interactive
+   (let ((old (sepia-thing-at-point 'symbol)))
+     (list (read-string "Old name: " old nil old)
+           (read-string "New name: ")
+           current-prefix-arg)))
+   (message "(%s %s)" old new)
+   (unless (sepia-inside-defun)
+     (error "Can't rename %s outside a defun." old))
+   (setq old (concat "\\([$%@]\\)\\_<" (regexp-quote old) "\\_>")
+         new 
+        (concat "\\1" new))
+   (let ((bod (sepia-beginning-of-defun))
+         (eod (sepia-end-of-defun)))
+     (if prompt
+       (query-replace-regexp old new nil bod eod)
+       (replace-regexp old new nil bod eod))))
 
 (defun sepia-defun-around-point (&optional where)
   "Return the text of function around point."
